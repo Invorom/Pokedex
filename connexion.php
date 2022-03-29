c19b7c5e89ba92de99068c3e68b8bc3d354251ba
Romain <romain.nerot@outlook.fr>
2022-03-29 17:59:57 +0200

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -16,19 +16,19 @@
         </form>
         <form action="verification_inscription.php" method="POST" enctype="multipart/form-data">
             <h1>Inscription</h1>
-            <div class="mb-3">
-                <label class="form-label">Votre email</label>
-                <input class="form-control" type="email" name="register_username" value="<?php echo isset($_COOKIE['register_cookie']) ? $_COOKIE['register_cookie'] : '';?>" placeholder="Username" required>
+            <div>
+                <label>Votre email</label>
+                <input type="email" name="register_username" value="<?php echo isset($_COOKIE['register_cookie']) ? $_COOKIE['register_cookie'] : '';?>" placeholder="Username" required>
             </div>
-            <div class="mb-3">
-                <label class="form-label">Votre email</label>
-                <input class="form-control" type="password" name="register_password" value="" placeholder="password" required>
+            <div>
+                <label>Votre email</label>
+                <input type="password" name="register_password" value="" placeholder="password" required>
             </div>
-            <div class="mb-3">
-                <label class="form-label">Votre image de profile</label>
-                <input class="form-control" type="file" name="image" accept="image/gif, image/png, image/jpeg">
+            <div>
+                <label>Votre image de profile</label>
+                <input type="file" name="image" accept="image/gif, image/png, image/jpeg">
             </div>
-            <input class="btn btn-primary" name="register_connexion" value="S'inscrire">
+            <input name="register_connexion" value="S'inscrire">
         </form>
     </main>
     <?php include('includes/Footer.php');?>
