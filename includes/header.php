ea9169f206063846e7733ef4715bec383a3eab9e
Marc <wilmot.marc@yahoo.fr>
2022-03-29 16:57:04 +0200

    Ajout modifs

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,12 @@
         <img src="images/logo.png" alt="Logo site pokedex">
         <nav>
             <ul>
-                <li>Accueil</li>
+                <li>
+                    <a href="index.html" title="Aller sur la page d'accueil">Accueil</a>
+                </li>
+                <li>
+                    <a href="collection.php.html" title="Aller sur la page d'accueil">Accueil</a>
+                </li>
             </ul>
         </nav>
     </div>
