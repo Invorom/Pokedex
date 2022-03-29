1c14f52567792c0439621e50576aa5f036313ebd
Marc <wilmot.marc@yahoo.fr>
2022-03-29 16:59:20 +0200

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,13 +1,13 @@
 <header>
     <div class="container">
-        <img src="images/logo.png" alt="Logo site pokedex">
+        <img src="images/logo.png" alt="Logo site pokedex" class="logo">
         <nav>
             <ul>
                 <li>
                     <a href="index.html" title="Aller sur la page d'accueil">Accueil</a>
                 </li>
                 <li>
-                    <a href="collection.php.html" title="Aller sur la page d'accueil">Accueil</a>
+                    <a href="collection.php.html" title="Aller sur la page de collection">Collection</a>
                 </li>
             </ul>
         </nav>
