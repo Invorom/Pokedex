84bcc10c5514fd15b19c64564690a38bde63581c
Marc <wilmot.marc@yahoo.fr>
2022-03-29 15:23:40 +0200

    Modification de l'index.php

--- index.php
+++ index.php
@@ -11,6 +11,12 @@ session_start();
 </head>
 <body>
     <?php include ('includes/header.php'); ?>
+
+    <main>
+
+    </main>
+
+    <?php include ('includes/footer.php'); ?>
     
 </body>
 </html>
