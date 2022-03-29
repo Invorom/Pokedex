6fac234c0729cb9cf380c6967b637672070ef7c2
johan <johanribl@gmail.com>
2022-03-29 17:00:27 +0200

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,6 @@
 <header>
     <div class="container">
-        <img class="logo" src="images/logo.png" alt="Logo site pokedex">
+        <img src="images/logo.png" alt="Logo site pokedex">
         <nav>
             <ul>
                 <li>Accueil</li>
