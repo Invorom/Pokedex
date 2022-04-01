59263b733f2b211aff58b774a262afe1a766bf2e
johan <johanribl@gmail.com>
2022-04-01 17:32:22 +0200

    done nice and clean

---
+++ collection.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="fr">
+    
+<?php
+$title="Accueil";
+include ('includes/head.php');
+?>
+
+<body>
+    <?php include ('includes/header.php'); ?>
+
+    <main>
+        <h1 class="center">TOUS LES POKEMONS</h1>
+        <div class="display-pok">
+        <?php
+
+            include('includes/db.php');
+            
+            $q = 'SELECT nom,pv,attaque,defense,vitesse,image FROM pokemon;';
+            $req = $bdd->prepare($q);
+            $req->execute();
+            $results = $req->fetchAll();
+            $count = 0;
+
+            //var_dump($results);
+            echo '<div class="row">';
+            foreach ($results as $key => $value){
+                include('includes/pokemon_display.php');
+                if($count%3 == 2){
+                    echo '</div><div class="row">';
+                }
+                $count +=1;
+            }
+            echo '</div>';
+        
+        ?>
+        </div>
+    </main>
+
+    <?php include ('includes/footer.php'); ?>
+    
+</body>
+</html>
---
+++ includes/pokemon_display.php
@@ -0,0 +1,20 @@
+<div class="row cell">
+    <div class="little-cell">
+        <table>
+            <?php 
+                echo '<tr><th>' . htmlspecialchars($results[$key]['nom']) . '</th></tr>';
+                echo '<tr><td>PV = ' . htmlspecialchars($results[$key]['pv']) . '</td></tr>';
+                echo '<tr><td>Attaque = ' . htmlspecialchars($results[$key]['attaque']) . '</td></tr>';
+                echo '<tr><td>Défense = ' . htmlspecialchars($results[$key]['defense']) . '</td></tr>';
+                echo '<tr><td>Vitesse = ' . htmlspecialchars($results[$key]['vitesse']) . '</td></tr>';
+            ?>
+        </table>
+    </div>
+    <div class="little-cell">
+        <?php 
+            echo '<img class="image-size" src="uploads_pokemon/' . htmlspecialchars($results[$key]['image']) . '" alt=" NOT FOUND">';
+        ?>
+
+    </div>
+
+</div>
