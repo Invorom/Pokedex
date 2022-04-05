9cd6fbfaaa53ef6accb623ee8e46d32d94f4f269
Opixelum <12w0zvkgazd3@opayq.net>
2022-04-05 20:22:05 +0200

    Refactored header CSS

--- includes/header.php
+++ includes/header.php
@@ -1,26 +1,24 @@
 <header>
-    <div class="container div-header">
-        <img class="logo" src="images/logo.png" alt="Logo site pokedex" class="logo">
-        <nav>
-            <ul class="display-row">
-                <li>
-                    <a href="index.php" title="Aller sur la page d'accueil">Accueil</a>
-                </li>
-                <li>
-                    <a href="collection.php" title="Aller sur la page de collection">Collection</a>
-                </li>
-            
-                <?php
-                    if (isset($_SESSION['email']))
-                    {
-                        echo '<li><a href="add_pokemon.php" title="Ajouter un Pokemon au Pokedex">Ajouter un Pokemon</a></li>';
-                        echo '<li><a href="profile.php" title="Consulter mon profil">Mon Compte</a></li>';
-                        echo '<li><a href="deconnexion.php" title="Se déconnecter">Déconnexion</a></li>';
-                    }
-                    else
-                        echo '<li><a href="connexion.php" title="Connexion">Connexion</a></li>';
-                ?>
-            </ul>
-        </nav>
-    </div>
+    <img class="logo" src="images/logo.png" alt="Logo site pokedex" class="logo">
+    <nav>
+        <ul>
+            <li>
+                <a href="index.php" title="Aller sur la page d'accueil">Accueil</a>
+            </li>
+            <li>
+                <a href="collection.php" title="Aller sur la page de collection">Collection</a>
+            </li>
+        
+            <?php
+                if (isset($_SESSION['email']))
+                {
+                    echo '<li><a href="add_pokemon.php" title="Ajouter un Pokemon au Pokedex">Ajouter un Pokemon</a></li>';
+                    echo '<li><a href="profile.php" title="Consulter mon profil">Mon Compte</a></li>';
+                    echo '<li><a href="deconnexion.php" title="Se déconnecter">Déconnexion</a></li>';
+                }
+                else
+                    echo '<li><a href="connexion.php" title="Connexion">Connexion</a></li>';
+            ?>
+        </ul>
+    </nav>
 </header>
