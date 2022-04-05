1446c3a77b0fe880978abd61da25e3ef5ed1a67d
Opixelum <12w0zvkgazd3@opayq.net>
2022-04-05 19:49:01 +0200

    Updated header

---
+++ add_pokemon.php
@@ -0,0 +1,17 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="fr" class="one-page">
+  <?php
+    $title="Accueil";
+    include ('includes/head.php');
+  ?>
+
+  <body>
+    <?php include ('includes/header.php'); ?>
+
+    <main>
+    </main>
+
+    <?php include ('includes/footer.php'); ?>
+  </body>
+</html>
--- includes/header.php
+++ includes/header.php
@@ -11,17 +11,16 @@
                 </li>
             
                 <?php
-                    if (isset($_SESSION['email'])){
+                    if (isset($_SESSION['email']))
+                    {
+                        echo '<li><a href="add_pokemon.php" title="Ajouter un Pokemon au Pokedex">Ajouter un Pokemon</a></li>';
+                        echo '<li><a href="profile.php" title="Consulter mon profil">Mon Compte</a></li>';
                         echo '<li><a href="deconnexion.php" title="Se déconnecter">Déconnexion</a></li>';
-                        echo '<li><a href="profile.php" title="Aller sur la page de profile">Mon Compte</a></li>';
-                    } else {
-                        echo '<li><a href="connexion.php" title="Connexion">Connexion</a></li>';
                     }
-
+                    else
+                        echo '<li><a href="connexion.php" title="Connexion">Connexion</a></li>';
                 ?>
             </ul>
-
         </nav>
     </div>
-
 </header>
