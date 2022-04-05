cd3db510d89638f2283ce33275671f31cdc95b14
Opixelum <12w0zvkgazd3@opayq.net>
2022-04-05 22:17:32 +0200

    Set message font color to red

--- includes/message.php
+++ includes/message.php
@@ -1,5 +1,5 @@
 <?php
     if(isset($_GET['message']) && !empty($_GET['message'])){
-        echo '<div><p>' .htmlspecialchars($_GET['message']). '</p></div>';
+        echo '<div><p class="message">' .htmlspecialchars($_GET['message']). '</p></div>';
     }
 ?>
